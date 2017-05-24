3cea503cdfdeafdb954337e59083ab0fb05e0bf5
akradin <alex.kradin@gmail.com>
2017-05-24 17:10:53 -0400

    Got carousel to work without referencing the script directly in the HTML
    Took out script tag in `index.html`, required the entire `carousel.js` file
    in `index.js` and it appears to be working

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,8 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const clickhandlers = require('./clickhandlers')
-import ScrollReveal from 'scrollreveal';
+const carousel = require('./carousel')
+import ScrollReveal from 'scrollreveal'
 
 $(() => {
   setAPIOrigin(location, config)
@@ -43,9 +44,4 @@ $(() => {
   });
 
 })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
 require('./example')
