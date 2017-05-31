8c3e39714f19d3dfbbe9f24bcbc19896fd83f6b7
akradin <alex.kradin@gmail.com>
2017-05-31 11:24:42 -0400

    Refactor all hover and click events into `clickhandlers.js`
    Set up a function to handle hover when on mobile for the about tab. The
    function determines window width and if it is `770px` or smaller it will
    change `mouseenter` / `mouseleave` events to `click`.
    
    Also moved the code to make this happen for mobile contact into
    `clickhandlers.js`

--- assets/scripts/clickhandlers.js
+++ assets/scripts/clickhandlers.js
@@ -1,6 +1,62 @@
 'use strict';
 
+// vars for functions below
+
+let aboutTab = document.querySelector(".svg-about-tab");
+let about = document.querySelector(".about");
+let aboutArrow = document.querySelector(".svg-about-tab-base svg polygon:nth-of-type(2)");
+let aboutArrowDesktop = document.querySelector(".svg-about-tab-base-desktop svg polygon:nth-of-type(2)");
+let contactMobileArrow = document.querySelector(".contact-btn-mobile svg");
+let contactMobileBtn = document.querySelector(".contact-btn-mobile");
+let contactMobileDropDown = document.querySelector(".contact-wrapper");
 let position = 0;
+
+// handle about section opening on click or hover depending on mobile or desktop
+function aboutTabOpen(event){
+  // highlight the mouseenter target
+  about.style.height = "630px";
+  $(".svg-about-tab-base-desktop").offset({top:"-60;"});
+  aboutArrow.classList.remove("bounce-class");
+  // aboutArrow.style.transform = 'rotateX(180deg)'
+  aboutArrowDesktop.classList.remove("bounce-class");
+}
+
+// handle about section closing on click or hover depending on mobile or desktop
+function aboutTabClose(event){
+    // highlight the mouseenter target
+    about.style.height = "0px";
+    $(".svg-about-tab-base-desktop").offset({top:"0;"});
+    aboutArrow.classList.add("bounce-class");
+    aboutArrowDesktop.classList.add("bounce-class");
+}
+
+// handle about section opening on click mobile
+function contactOpen(event){
+  contactMobileDropDown.style.height = "400px";
+  contactMobileArrow.classList.remove("bounce-class");
+}
+
+// handle about section closing on click mobile
+function contactClose(event){
+  contactMobileDropDown.style.height = "0px";
+  contactMobileArrow.classList.add("bounce-class");
+}
+
+const clickOrHoverAbout = () => {
+  if ($(window).width <= 770){
+    $(aboutTab).on('click', aboutTabOpen);
+    $(aboutTab).on('click', aboutTabClose)
+  } else {
+    $(aboutTab).on('mouseenter', aboutTabOpen);
+    $(aboutTab).on('mouseleave', aboutTabClose);
+  }
+}
+
+const clickOrHoverContact = () =>{
+    $(contactMobileBtn).on('click', contactOpen)
+    $(contactMobileDropDown).on('click', contactClose)
+}
+
 function nextSlide(){
     position -= 184;
     let leftStyle = position;
@@ -25,5 +81,7 @@ const addHandlers = () => {
 
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  clickOrHoverAbout,
+  clickOrHoverContact,
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -19,64 +19,19 @@ $(() => {
   sr.reveal('.sr');
   sr.reveal('.what-we-buy .container');
 
-  // add clickhandlers for mobile what we buy
+  // add clickhandlers/hover functions
   clickhandlers.addHandlers();
+  clickhandlers.clickOrHoverAbout();
+  clickhandlers.clickOrHoverContact();
 
   let aboutArrow = document.querySelector(".svg-about-tab-base svg polygon:nth-of-type(2)");
   let aboutArrowDesktop = document.querySelector(".svg-about-tab-base-desktop svg polygon:nth-of-type(2)");
   let contactMobileArrow = document.querySelector(".contact-btn-mobile svg");
-  let contactMobileBtn = document.querySelector(".contact-btn-mobile");
-  let contactMobileDropDown = document.querySelector(".contact-wrapper");
 
 
+  // ARROW ANIMATION
   aboutArrow.classList.add("bounce-class");
   aboutArrowDesktop.classList.add("bounce-class");
   contactMobileArrow.classList.add("bounce-class");
-  // aboutArrowDesktop.style.top = "0px";
   $(".svg-about-tab-base-desktop").offset({top:"0;"});
-
-// add hover effect to about section
-  let aboutTab = document.querySelector(".svg-about-tab");
-  let about = document.querySelector(".about");
-
-
-  // this handler will be executed only once when the cursor moves over the unordered list
-  aboutTab.addEventListener("mouseenter", function( event )   {
-    // highlight the mouseenter target
-    about.style.height = "630px";
-    // aboutArrowDesktop.style.top = "-60px";
-    $(".svg-about-tab-base-desktop").offset({top:"-60;"});
-    aboutArrow.classList.remove("bounce-class");
-    aboutArrowDesktop.classList.remove("bounce-class");
-  });
-
-  aboutTab.addEventListener("mouseout", function( event )   {
-    // highlight the mouseenter target
-    about.style.height = "0px";
-    // aboutArrowDesktop.style.top = "0px";
-    $(".svg-about-tab-base-desktop").offset({top:"0;"});
-    aboutArrow.classList.add("bounce-class");
-    aboutArrowDesktop.classList.add("bounce-class");
-  });
-
-
-// MOBILE CONTACT ARROW ANIMATION
-  contactMobileBtn.addEventListener("mouseenter", function( event )   {
-    // highlight the mouseenter target
-    contactMobileDropDown.style.height = "400px";
-    contactMobileArrow.classList.remove("bounce-class");
-  });
-
-  contactMobileBtn.addEventListener("mouseout", function( event )   {
-    // highlight the mouseout target
-    contactMobileDropDown.style.height = "0px";
-    contactMobileArrow.classList.add("bounce-class");
-  });
-
 })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-require('./example')
