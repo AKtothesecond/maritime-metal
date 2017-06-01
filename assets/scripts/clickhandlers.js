215074dc48d4d0269b82eb95c012149068563382
akadane87 <akadane87@gmail.com>
2017-06-01 10:11:38 -0400

    mobile About Tab animate function: unresolved  safari white to transparent gradient: resolved  what we buy mobile: resolved
     about tab function: need to query browser width,
     compare to break points, and correct event and style change accordingly
    
     safari gradient: revized syntax based on version bug
    
     mobile carousel and background now showing in proper media queries

--- assets/scripts/clickhandlers.js
+++ assets/scripts/clickhandlers.js
@@ -3,6 +3,8 @@
 // vars for functions below
 
 let aboutTab = document.querySelector(".svg-about-tab");
+let aboutTabBase = document.querySelector(".svg-about-tab-base");
+let aboutTabBaseDesktop = document.querySelector(".svg-about-tab-base-desktop");
 let about = document.querySelector(".about");
 let aboutArrow = document.querySelector(".svg-about-tab-base svg polygon:nth-of-type(2)");
 let aboutArrowDesktop = document.querySelector(".svg-about-tab-base-desktop svg polygon:nth-of-type(2)");
@@ -10,24 +12,44 @@ let contactMobileArrow = document.querySelector(".contact-btn-mobile svg");
 let contactMobileBtn = document.querySelector(".contact-btn-mobile");
 let contactMobileDropDown = document.querySelector(".contact-wrapper");
 let position = 0;
+let count = 0;
+
+// const displayOne = () => {
+//   $('.svg-about-tab-base').css('top', '10px');
+//   $('.about').css('height', '630px');
+// }
 
 // handle about section opening on click or hover depending on mobile or desktop
 function aboutTabOpen(event){
   // highlight the mouseenter target
   about.style.height = "630px";
-  $(".svg-about-tab-base-desktop").offset({top:"-60;"});
   aboutArrow.classList.remove("bounce-class");
-  // aboutArrow.style.transform = 'rotateX(180deg)'
   aboutArrowDesktop.classList.remove("bounce-class");
+
+  if($(window).width() >= 450 && $(window).width() <= 1100 ){
+    $(aboutTabBase).css("top", "-105px")
+  } else if($(window).width() <= 450){
+    $(aboutTabBase).css("top", "-80px")
+  } else {
+    $(aboutTabBaseDesktop).css("top", "-75px")
+  }
+  count ++
 }
 
 // handle about section closing on click or hover depending on mobile or desktop
 function aboutTabClose(event){
     // highlight the mouseenter target
     about.style.height = "0px";
-    $(".svg-about-tab-base-desktop").offset({top:"0;"});
     aboutArrow.classList.add("bounce-class");
     aboutArrowDesktop.classList.add("bounce-class");
+    if($(window).width() >= 450 && $(window).width() <= 1100 ){
+      $(aboutTabBase).css("top", "-75px")
+    } else if($(window).width() <= 450){
+      $(aboutTabBase).css("top", "-10px")
+    } else {
+      $(aboutTabBaseDesktop).css("top", "5px")
+    }
+    count ++
 }
 
 // handle about section opening on click mobile
@@ -42,10 +64,64 @@ function contactClose(event){
   contactMobileArrow.classList.add("bounce-class");
 }
 
+/////// HOW I THINK ABOUT EVENTS SHOULD RESEMBLE //////
+
+// const displayTablet = () => {
+//   $('.svg-about-tab-base').css('top', '10px');
+//   $('.about').css('height', '630px');
+// }
+
+// const displayMobile = () => {
+//   $('.svg-about-tab-base').css('top', '-80px');
+//   $('.about').css('height', '630px');
+// }
+
+// const aboutEvents = () => {
+//   if ($(window).width() <= 1100){
+//       $(aboutTab).click(function() {
+//         $('.about').toggle( // YOU COULD PASS displayTablet(); TO BE TOGGLED
+//         $(this).css('top','630px');
+//         $('.svg-about-tab-base').css('height','10px');
+//       });
+//   }
+//   else if ($(window).width() <= 450){
+//     $(aboutTab).click(function() {
+//       $('.about').toggle( // YOU COULD PASS displayMobile(); TO BE TOGGLED
+//       $(this).css('top','630px');
+//       $('.svg-about-tab-base').css('height','10px');
+//     });
+//   }
+//   else {
+//     $(aboutTab).on('mouseenter', aboutTabOpen);
+//     $(aboutTab).on('mouseleave', aboutTabClose);
+//   }
+// };
+
+////// You could toggleClass with these CSS classes ////////////////
+// .about-open-tablet {
+//   .about{
+//   height: 630px;
+// }
+//   &.services .svg-about-tab-base{
+//     top: 10px;
+//   }
+// }
+
+// .about-open-mobile {
+//   .about{
+//   height: 630px;
+// }
+//   &.services .svg-about-tab-base{
+//     top: 10px;
+//   }
+// }
+//////////////////// end about events //////////////////////
+
+
 const clickOrHoverAbout = () => {
-  if ($(window).width <= 770){
+  if ($(window).width() <= 1100){
     $(aboutTab).on('click', aboutTabOpen);
-    $(aboutTab).on('click', aboutTabClose)
+    // $(aboutTab).on('click', aboutTabClose)
   } else {
     $(aboutTab).on('mouseenter', aboutTabOpen);
     $(aboutTab).on('mouseleave', aboutTabClose);
